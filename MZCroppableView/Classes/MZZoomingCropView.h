48d2dadbc44487520adc6f49d9a6fac78292ea02
Jeremy Lawrence <Jeremy.Y.Lawrence@gmail.com>
2014-07-08 16:13:05 -0700

    Added Zooming Crop View to replace CroppingScrollView
    Putting a cropping image view into a scroll view proved to be difficult with
    trying to keep the imageview centered properly while rotating.

---
+++ MZCroppableView/Classes/MZZoomingCropView.h
@@ -0,0 +1,35 @@
+//
+//  MZZoomingCropView.h
+//  MZCroppableView
+//
+//  Created by Jeremy Lawrence on 7/3/14.
+//  Copyright (c) 2014 macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MZCroppingImageView.h"
+
+@class MZZoomingCropView;
+@protocol MZZoomingCropViewDelegate <NSObject>
+
+@optional
+- (void)touchesBeganOnZoomingCropView:(MZZoomingCropView *)cropView;
+- (void)touchesEndedOnZoomingCropView:(MZZoomingCropView *)cropView;
+
+@end
+
+@interface MZZoomingCropView : UIView
+
+@property (weak, nonatomic) id<MZZoomingCropViewDelegate> delegate;
+@property (strong, nonatomic) UIImage *image;
+@property (weak, nonatomic) MZCroppingImageView *imageView;
+
+@property (nonatomic) CGFloat cropLineWidth;
+@property (nonatomic, getter = isCropEnabled) BOOL cropEnabled;
+
+/**
+ Returns the image cropped to the path defined by the user's drawing.
+ */
+- (UIImage *)getCroppedImage;
+
+@end
---
+++ MZCroppableView/Classes/MZZoomingCropView.m
@@ -0,0 +1,173 @@
+//
+//  MZZoomingCropView.m
+//  MZCroppableView
+//
+//  Created by Jeremy Lawrence on 7/3/14.
+//  Copyright (c) 2014 macbook. All rights reserved.
+//
+
+#import "MZZoomingCropView.h"
+
+@interface MZZoomingCropView()
+<MZCroppingImageViewDelegate>
+
+@end
+
+@implementation MZZoomingCropView
+
+- (void)_commonInitializer
+{
+    UIPinchGestureRecognizer *pinch = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(scaleImageView:)];
+    [self addGestureRecognizer:pinch];
+    
+    UIRotationGestureRecognizer *rotate = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(rotateImageView:)];
+    [self addGestureRecognizer:rotate];
+    
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(translateImageView:)];
+    [self addGestureRecognizer:pan];
+    
+    self.cropEnabled = NO;
+    self.cropLineWidth = 5.0f;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        [self _commonInitializer];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self _commonInitializer];
+    }
+    return self;
+}
+
+- (instancetype)initWithImage:(UIImage *)image
+{
+    CGRect frame = { CGPointMake(0, 0), image.size };
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self _commonInitializer];
+        [self setImage:image];
+    }
+    return self;
+}
+
+- (void)setImage:(UIImage *)image
+{
+    _image = image;
+    
+    // Clean up previous imageView
+    [self.imageView removeFromSuperview];
+    self.imageView = nil;
+    
+    MZCroppingImageView *imageView = [[MZCroppingImageView alloc] initWithImage:image];
+    [imageView setDelegate:self];
+    [self addSubview:imageView];
+    self.imageView = imageView;
+    
+    [self initialImageCentering];
+}
+
+- (void)initialImageCentering
+{
+    // Scale imageview to fit the cropping view
+    CGFloat fittingScale = MIN((self.bounds.size.width/self.imageView.image.size.width),
+                               (self.bounds.size.height/self.imageView.image.size.height));
+    if (fittingScale < 1) {
+        self.imageView.transform = CGAffineTransformScale(self.imageView.transform, fittingScale, fittingScale);
+    }
+    
+    // Move the image to the center of the view
+    CGFloat centeredX = (self.bounds.size.width - self.imageView.frame.size.width)/2;
+    CGFloat centeredY = (self.bounds.size.height - self.imageView.frame.size.height)/2;
+    [self.imageView setFrame:(CGRect){CGPointMake(centeredX, centeredY), self.imageView.frame.size}];
+}
+
+- (CGFloat)currentScaleFactor
+{
+    CGFloat scaleFactor = sqrt( pow(self.imageView.transform.a,2) + pow(self.imageView.transform.c,2));
+    return scaleFactor;
+}
+
+#pragma mark - Gestures
+
+- (void)scaleImageView:(UIPinchGestureRecognizer *)gesture
+{
+    CGFloat scale = gesture.scale;
+    if (!self.isCropEnabled) {
+        self.imageView.transform = CGAffineTransformScale(self.imageView.transform, scale, scale);
+    }
+    gesture.scale = 1.0;
+}
+
+- (void)rotateImageView:(UIRotationGestureRecognizer *)gesture
+{
+    CGFloat angle = gesture.rotation;
+    if (!self.isCropEnabled) {
+        self.imageView.transform = CGAffineTransformRotate(self.imageView.transform, angle);
+    }
+    gesture.rotation = 0.0;
+}
+
+- (void)translateImageView:(UIPanGestureRecognizer *)gesture
+{
+    CGPoint translation = [gesture translationInView:self];
+    CGPoint imageViewCenter = self.imageView.center;
+    
+    imageViewCenter.x += translation.x;
+    imageViewCenter.y += translation.y;
+    
+    if (!self.isCropEnabled) {
+        self.imageView.center = imageViewCenter;
+    }
+    
+    [gesture setTranslation:CGPointZero inView:self];
+}
+
+#pragma mark - Image cropping
+
+- (UIImage *)getCroppedImage
+{
+    return [self.imageView getCroppedImage];
+}
+
+- (void)setCropEnabled:(BOOL)cropEnabled
+{
+    _cropEnabled = cropEnabled;
+    [self.imageView setUserInteractionEnabled:cropEnabled];
+}
+
+#pragma mark - MZCroppingImageViewDelegate
+
+- (void)touchesBeganOnCroppingImageView:(MZCroppingImageView *)croppingImageView
+{
+    NSLog(@"Setting line width");
+    CGFloat zoomScale = [self currentScaleFactor];
+    CGFloat containerWidth = self.bounds.size.width;
+    CGFloat desiredLine = self.cropLineWidth;
+    CGFloat screenWidth = [[UIScreen mainScreen] bounds].size.width;
+    CGFloat newLineWidth = (containerWidth / zoomScale) * (desiredLine/screenWidth);
+    NSLog(@"New line width: %f", newLineWidth);
+    [self.imageView.cropView.croppingPath setLineWidth:newLineWidth];
+
+    if ([self.delegate respondsToSelector:@selector(touchesBeganOnZoomingCropView:)]) {
+        [self.delegate touchesBeganOnZoomingCropView:self];
+    }
+}
+
+- (void)touchesEndedOnCroppingImageView:(MZCroppingImageView *)croppingImageView
+{
+    if ([self.delegate respondsToSelector:@selector(touchesEndedOnZoomingCropView:)]) {
+        [self.delegate touchesEndedOnZoomingCropView:self];
+    }
+}
+
+@end
