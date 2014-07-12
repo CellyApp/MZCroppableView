a61d5e66b78024f60e29cde7a7e79c01c3e8cf9a
Jeremy Lawrence <Jeremy.Y.Lawrence@gmail.com>
2014-07-11 17:58:01 -0700

    Removed code no longer used in croppableView

--- MZCroppableView/Classes/MZCroppableView.h
+++ MZCroppableView/Classes/MZCroppableView.h
@@ -27,26 +27,4 @@
 
 - (id)initWithImageView:(UIImageView *)imageView;
 
-+ (CGRect)scaleRespectAspectFromRect1:(CGRect)rect1 toRect2:(CGRect)rect2;
-
-/**
- Creates an image from the given imageView's image and the path drawn in the
- MZCroppableView.
- 
- @param imageView  The UIImageView from which the new, cropped image is to be
- generated
- @param imageFrame The frame in which the image is actually displayed, relative
- to the imageView's bounds.
- @param drawnPath  Whether or not the path being used to crop the image was
- drawn by a user or is a pre-set or generated path. This parameter determines
- if the path used for cropping should be smoothed or not. User-drawn paths are
- generally jagged and thus require smoothing for an appealing image crop.
- Pre-generated crop shapes can be broken by the smoothing algorithm, resulting
- in crashes when trying to crop to an invalid path.
- 
- @return An image cropped to the path drawn in the MZCroppableView.
- */
-- (UIImage *)grabCroppedImageFromImageView:(UIImageView *)imageView
-                       displayedImageFrame:(CGRect)imageFrame
-                             fromDrawnPath:(BOOL)drawnPath;
 @end
